efd6657a6f081b5e86be564b98527da54154c7f9
Starsky <starsky.m@live.com>
2022-02-07 07:30:08 +0100

    Initial Commit

---
+++ VideoPlayerXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayerXcode
+//
+//  Created by star on 07.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
