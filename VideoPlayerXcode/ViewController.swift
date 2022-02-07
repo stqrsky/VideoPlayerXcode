db2736279adb15ac523678eaab813b1aadcbb510
Starsky <starsky.m@live.com>
2022-02-07 09:59:43 +0100

    UI + Actions and Outlets

--- VideoPlayerXcode/ViewController.swift
+++ VideoPlayerXcode/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playButton.layer.cornerRadius = 20
     }
 
-
+    
+    @IBAction func playButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
